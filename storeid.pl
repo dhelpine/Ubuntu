0f85dbffe5073ae8a532280387d48e86a733ac42
dhelpine <dhelpine@users.noreply.github.com>
2016-10-04 10:09:31 +0700

    Update storeid.pl

--- storeid.pl
+++ storeid.pl
@@ -1,20 +1,19 @@
-cat > /etc/squid/storeid.pl <<- 'selesai'
 #!/usr/bin/perl
- 
+
 $|=1;
 while (<>) {
 @X = split;
 if ($X[0] =~ m/^https?\:\/\/.*/) {
-        $x = $X[0];
-        $_ = $X[0];
-        $u = $X[0];
-} else {
-        $x = $X[1];
-        $_ = $X[1];
-        $u = $X[1];
+	$x = $X[0]; 
+	$_ = $X[0];
+	$u = $X[0];
+} else { 
+	$x = $X[1]; 
+	$_ = $X[1];
+	$u = $X[1];
 }
- 
- 
+
+
 #ads youtube
 if ($x=~ m/^https?\:\/\/.*youtube.*api.*stats.*ads.*/){
     @content_v = m/[&?]content_v\=([^\&\s]*)/;
@@ -25,7 +24,7 @@ if ($x=~ m/^https?\:\/\/.*youtube.*api.*stats.*ads.*/){
     close FILE;
     }
     $out=$x;
- 
+
 #tracking youtube
 } elsif ($x=~ m/^https?\:\/\/.*youtube.*(ptracking|set_awesome).*/){
     @video_id = m/[&?]video_id\=([^\&\s]*)/;
@@ -36,7 +35,6 @@ if ($x=~ m/^https?\:\/\/.*youtube.*api.*stats.*ads.*/){
     close FILE;
     }
     $out=$x;
- 
  
 #stream_204 youtube
 } elsif ($x=~ m/^https?\:\/\/.*youtube.*(stream_204|watchtime|qoe|atr).*/){
@@ -59,14 +57,14 @@ if ($x=~ m/^https?\:\/\/.*youtube.*api.*stats.*ads.*/){
     close FILE;
     }
     $out=$x;
- 
+
 #youtube
 } elsif ($x=~ m/^https?\:\/\/.*(youtube|googlevideo).*videoplayback.*title.*/){
     @title      = m/[%&?\/](title[%&=\/][^\&\s\/]*)/;
     @itag      = m/[%&?\/](itag[%&=\/][^\&\s\/]*)/;
     @range   = m/[%&?\/](range[%&=\/][^\&\s\/]*)/;
     $out="http://pc-mikrotik/youtube/@itag@title@range";
-
+ 
 #youtube
 } elsif ($x=~ m/^https?\:\/\/.*(youtube|googlevideo).*videoplayback.*/){
     @cpn      = m/[%&?\/](cpn[%&=\/][^\&\s\/]*)/;
@@ -82,33 +80,34 @@ if ($x=~ m/^https?\:\/\/.*youtube.*api.*stats.*ads.*/){
         close FILE;}
     }
     $out="http://pc-mikrotik/youtube/@id@itag@mime@range@slices";
- 
- 
+
 #utmgif
 } elsif ($x=~ m/^https?\:\/\/.*utm.gif.*/) {
     $out="http://pc-mikrotik/__utm.gif";
- 
- 
+
 #safe_image FB
 } elsif ($x=~ m/^https?\:\/\/fbexternal-a\.akamaihd\.net\/safe_image\.php\?d.*/) {
     @d = m/[&?]d\=([^\&\s]*)/;
     @h = m/[&?]h\=([^\&\s]*)/;
     @w = m/[&?]w\=([^\&\s]*)/;
     $out="http://pc-mikrotik/safe_image/d=@d&w=@w&h=@h";
- 
-###
+
+#safe_image FB 2
 } elsif ($x=~ m/^https?\:\/\/fbstatic-a\.akamaihd\.net\/safe_image\.php\?d.*/) {
-    $out="http://agunggz/safe_image/d=@d&w=@w&h=@h";
-###
- 
+    $out="http://pc-mikrotik/safe_image/d=@d&w=@w&h=@h";
+
 #fbcdn size picture
 } elsif ($x=~ m/^https?\:\/\/.*(fbcdn).*\/v\/.*\/(.*x.*\/.*\.(jpg|jpeg|bmp|ico|png|gif))\?oh=\.*/) {
     $out="http://pc-mikrotik/fbcdn/" . $2;
- 
+
 #fbcdn picture
 } elsif ($x=~ m/^https?\:\/\/.*(fbcdn).*\/v\/.*\/(.*\.(jpg|jpeg|bmp|ico|png|gif))\?oh=\.*/) {
     $out="http://pc-mikrotik/fbcdn/" . $2;
- 
+
+#Facebook
+#} elsif ($x=~ m/^https?\:\/\/.*(fbcdn|akamaihd)\.net\/(.*)\/(([aA-zZ][\d]*[x][\d]*)\/(.*)(\.[\w]{2,4}))|\.(jpg|jpeg|bmp|ico|png|gif)\?oh=\.*/) {
+#    $out="http://pc-mikrotik/fbcdn/" . $2;
+
 #reverbnation
 } elsif ($x=~ m/^https?\:\/\/c2lo\.reverbnation\.com\/audio_player\/ec_stream_song\/(.*)\?.*/) {
     $out="http://pc-mikrotik/reverbnation/" . $1;
@@ -116,60 +115,63 @@ if ($x=~ m/^https?\:\/\/.*youtube.*api.*stats.*ads.*/){
 #playstore
 } elsif ($x=~ m/^https?\:\/\/.*\.c\.android\.clients\.google\.com\/market\/GetBinary\/GetBinary\/(.*\/.*)\?.*/) {
     $out="http://pc-mikrotik/android/market/" . $1;
- 
+
 #filehost
 } elsif ($x=~ m/^https?\:\/\/.*datafilehost.*\/get\.php.*file\=(.*)/) {
     $out="http://pc-mikrotik/filehost/" . $1;
- 
+
 #speedtest
 } elsif ($x=~ m/^https?\:\/\/.*(speedtest|espeed).*\/(.*\.(jpg|txt|png|bmp)).*/) {
     $out="http://pc-mikrotik/speedtest/" . $2;
- 
+
 #filehippo
 } elsif ($x=~ m/^https?\:\/\/.*\.filehippo\.com\/.*\/(.*\/.*)/) {
     $out="http://pc-mikrotik/filehippo/" . $1;
- 
+
 #4shared preview.mp3
-} elsif ($x=~ /^https?\:\/\/.*\.4shared\.com\/.*\/(.*\/.*)\/dlink.*preview.mp3/) {
+} elsif ($x=~ m/^https?\:\/\/.*\.4shared\.com\/.*\/(.*\/.*)\/dlink.*preview.mp3/) {
     $out="http://pc-mikrotik/4shared/preview/" . $1;
- 
+
 #4shared
 } elsif ($x=~ m/^https?\:\/\/.*\.4shared\.com\/download\/(.*\/.*)\?tsid.*/) {
     $out="http://pc-mikrotik/4shared/download/" . $1;
 
-#steampowered dota 2
-} elsif ($x=~ m/^https?\:\/\/media\d+\.steampowered\.com\/client\/(.*)/) {
-    $out="http://pc-mikrotik/media/steampowered/" . $1;
+#imdb
+} elsif ($url=~ m/^https?\:\/\/video\-http\.media\-imdb\.com\/(.*\.mp4)\?.*/) {
+    $out="http://pc-mikrotik/imdb/$1";
 
-#steampowered dota2 chunk-manifest
-} elsif ($x=~ m/^https?\:\/\/valve\d+\.cs\.steampowered\.com\/depot\/(.*)/) {
-    $out="http://pc-mikrotik/steampowered/depot/" . $1;
+#sourceforge
+} elsif ($url=~ m/^https?\:\/\/.*\.dl\.sourceforge\.net\/(.*\.[\w]{2,3})/) {
+    $out="http://pc-mikrotik/sourceforge/$1";
+
+#steampowered dota2
+} elsif ($x=~ m/^https?\:\/\/.*\.steam(content|powered)\.com\/((depot|client)\/[\d]+\/chunk\/[\w]+)(\?.*)?/) {
+    $out="http://pc-mikrotik/steampowered/depot/" . $2;
 
 #animeindo
-} elsif ($x =~ m/^https?\:\/\/.*aisfile\.com:182\/.\/(.*)\/(.*\.(mp4|flv)).*/){
+} elsif ($x =~ m/^http:\/\/.*aisfile\.com:182\/.\/(.*)\/(.*\.(mp4|flv)).*/) {
     $out="store-id://aisfile:182.SQUIDINTERNAL/$2\n";
 
-#update-mozilla
-} elsif ($x =~ m/^http?\:\/\/.*google\.com\/safebrowsing\/.*/){
-    $out="http://pc-mikrotik/safebrowsing/$2\n";
-
 #vimeo-cdn
-} elsif ($x =~ m/^https:\/\/.*vimeocdn\.com\/(.*)\/(.*\.(mp4|m4s))/){
+} elsif ($x =~ m/^https:\/\/.*vimeocdn\.com\/(.*)\/(.*\.(mp4|m4s))/) {
     $out="http://pc-mikrotik/vimeo/$2\n";
 
-#vimeo-akamaihd
-} elsif ($x =~ m/^https:\/\/(skyfiregce-a\.akamaihd\.net)\/(.*\/.*)\/(video)\/(.*\.m4s|mp4)/){
-    $out="http://pc-mikrotik/akamaihd/$2\n";
+#kodi-video
+} elsif ($x =~ m/^https?\:\/\/.*(youtube|googlevideo)\.com\/(.*)/) {
+    $out="http://pc-mikrotik/youtube/$2\n";
+
+#google-photo
+#} elsif ($x =~ m/^https?\:\/\/.*googleusercontent\.com\/([\w]+[a-zA-Z]+).*/) {
+#    $out="http://pc-mikrotik/googleusercontent/" . $1;
 
 
 } else {
 $out=$x;
 }
- 
+
 if ($X[0] =~ m/^https?\:\/\/.*/) {
-        print "OK store-id=$out\n";
+	print "OK store-id=$out\n";
 } else {
-        print $X[0] . " " . "OK store-id=$out\n";
+	print $X[0] . " " . "OK store-id=$out\n";
 }
 }
-selesai
